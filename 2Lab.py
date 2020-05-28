9f6a3fadb9562197b185d1e2493b622768524e60
DaniilTS <48275635+DaniilTS@users.noreply.github.com>
2020-05-29 01:59:13 +0300

    Update 2Lab.py

--- 2Lab.py
+++ 2Lab.py
@@ -4,148 +4,180 @@ import matplotlib.patches as patches
 from math import sqrt
 import matplotlib.pyplot as plt
 import random
-from collections import Counter
+import math
 
 
 def random_Yi(a, b):
-    size = random.randint(4, 10)
-    size *= size
+    size = random.randint(80, 150)
+    print(size)
     size = 16
-    arr = np.array([random.random() for i in range(size)], float)  # Ksi_i
-    arr = np.dot(arr, b - a)
-    arr = arr + a  # Xi
+
+    arr = np.array([a + random.random() * (b - a) for i in range(size)], float)  # Ksi_i
     arr = [pow(i, 4) for i in arr]  # Yi
-    arr = np.around(arr,1)
+    arr = sorted(arr)
+    arr = np.around(arr, 1)
     return arr
 
 
-def draw_poligon(arr1, arr2):
-    x = np.copy(arr2)
-    y = sorted(list(set(arr1)))
-    pylab.subplot(2, 2, 1)
-    pylab.plot(x, y)
-    pylab.title("Полигон распределения")
+def plot_equal_interval_hist(variation_series, num_of_columns, A, B):
+    len_of_row = variation_series[-1] - variation_series[0]
+    print("Длина ряда", len_of_row)
+    len_of_column = len_of_row / num_of_columns
+    print("Ширина столбцов:", len_of_column)
 
-
-def draw_imper_func(arr1, arr2):
-    relative_freq_arr = arr1 / len(arr2)
-    # print("Относительные частоты:", relative_freq_arr)
-
-    j = 0
     sum = 0
-    summed_rel_freq = relative_freq_arr.copy()
-    for i in range(relative_freq_arr.size):
-        while i >= j:
-            sum += relative_freq_arr[j]
+    for i in range(num_of_columns):
+        A[i] = sum
+        B[i] = A[i] + len_of_column
+        sum += len_of_column
+
+    columns_height = np.zeros(num_of_columns)
+    j = 0
+    for i in variation_series:
+        if i == B[j] and i != variation_series[-1]:
+            columns_height[j] += 0.5
+            columns_height[j + 1] += 0.5
             j += 1
-        summed_rel_freq[i] = sum
-        sum = 0
-        j = 0
-    # print("Относительные накопленные частоты:", summed_rel_freq)
-    var_set_array = sorted(set(variation_series))
-    var_set_array = np.concatenate([[-100], var_set_array, [var_set_array[len(var_set_array) - 1] + 100]])
-
-    y = np.zeros(summed_rel_freq.size * 2)
-    counter = 0
-    k = 0
-    for i in range(summed_rel_freq.size):
-        y[k], y[k + 1] = summed_rel_freq[counter], summed_rel_freq[counter]
-        k += 2
-        counter += 1
-
-    x = np.zeros(len(var_set_array) * 2)
-    counter = 0
-    k = 0
-    for i in range(len(var_set_array)):
-        if k + 2 == len(var_set_array) * 2:
-            x[k], x[k + 1] = var_set_array[counter], var_set_array[counter] + 1
             continue
-        x[k], x[k + 1] = var_set_array[counter], var_set_array[counter + 1]
-        k += 2
-        counter += 1
-
-    y = np.concatenate([[0], y, [1]])
-    y = np.concatenate([[0], y, [1]])
-
-    pylab.subplot(2, 2, 2)
-    pylab.plot(x, y)
-    pylab.title("Эмпирическая функция")
-
-
-def draw_equal_interval_hist(arr):
-    arr1 = np.copy(arr)
-    num_of_columns = sqrt(len(arr))
-    print("Количество столбцов(равноинтервальный метод):", num_of_columns)
-    columns_len = (arr[-1] - arr[-0]) / num_of_columns
-    print("Длина интервала:", columns_len)
-    columns_height = np.zeros(int(num_of_columns))
-
-    right = arr[0] + columns_len
-    k = 0
-    for i in range(int(num_of_columns)):  # итерируемся M раз
-        while True:
-            if arr[k] <= right:
-                columns_height[i] += 1
-                k += 1
-                if k == len(arr):
-                    break
-            else:
-                right += columns_len
+        elif A[j] <= i <= B[j]:
+            columns_height[j] += 1
+        else:
+            if j == len(columns_height)-1:
                 break
+            columns_height[j + 1] += 1
+            j += 1
 
-    columns_height = columns_height.dot(1 / len(arr))
-    print("Высоты столбцов(равноинтервальный метод):", columns_height)
+    columns_height /= len(variation_series)
 
+    print("Высоты столбцов", columns_height)
+    print(np.sum(columns_height))
     fig, ax = plt.subplots()
-    # ax = pylab.subplots(2,2,3)
-    arr = [i*columns_len for i in range(int(num_of_columns))]
-    print(arr)
     for i in range(int(num_of_columns)):
         ax.add_patch(
             patches.Rectangle(
-                (arr[i], 0),
-                columns_len,
+                (A[i], 0),
+                len_of_column,
                 columns_height[i],
-                edgecolor="red"
+                edgecolor="red",
             )
         )
-    plt.xlim(-1, arr1[-1] + 10)
+
+    f = lambda x: (1 / (16 * np.power(x, 3 / 4)))
+    x = np.linspace(0.0005, 81)
+    plt.plot(x, f(x), "yellow")
+    plt.xlim(-1, variation_series[-1] + 1)
+    plt.ylim(0, 1)
+    plt.show()
+    return A, B
 
 
-def draw_equal_chance_hist(arr):
-    for i in arr:
-        if i==0:
-            arr = np.delete(arr, 0)
-    num_of_columns = int(sqrt(len(arr)))
-    num_of_elements_in_gap = int(len(arr)/num_of_columns)
-    gap_lengths = np.zeros(int(num_of_columns)-1)
-    gap_height = np.zeros(num_of_columns)
-    print(num_of_columns)
-    print(num_of_elements_in_gap)
-    counter = 0
-    print(sorted(list(set(arr) )))
-    for i in range(int(num_of_columns)-1):
-        gap_lengths[i] = (arr[counter+num_of_elements_in_gap-1]+arr[counter+num_of_elements_in_gap])/2 - arr[counter]
-        counter += num_of_elements_in_gap
-    gap_lengths = np.concatenate([[arr[0]], gap_lengths, [arr[-1]]])
-    print(gap_lengths)
+def plot_poligon_and_func(A, B, num_of_columns, variation_series):
+    x = np.zeros(num_of_columns)
+    for i in range(num_of_columns):
+        x[i] = A[i] + (B[i] - A[i]) / 2
 
+    y = np.zeros(num_of_columns)
+    uniq = []
+    j = 0
+    for i in variation_series:
+        if A[j] <= i <= B[j]:
+            if i not in uniq:
+                y[j] += 1
+                uniq.append(i)
+        else:
+            j += 1
+            uniq.append(i)
+            y[j] += 1
+    y /= len(variation_series)
+
+    pylab.subplot(1, 1, 1)
+    pylab.ylim(-0.1, y[0]+0.05)
+    pylab.xlim(-2, x[-1] + 5)
+    pylab.plot(x, y)
+    f = lambda x: (1 / (16 * np.power(x, 3 / 4)))
+    x = np.linspace(0.000005, 81)
+    pylab.plot(x, f(x), "yellow")
+    x, y = [-100, 0],[0, 0]
+    pylab.plot(x, y, "yellow")
+    x, y = [81, 100], [0, 0]
+    pylab.plot(x, y, "yellow")
+    plt.show()
+
+
+def plot_equal_chance_hist(M):
+    while True:
+        if len(variation_series) % M == 0:
+            num_of_elements_on_interval = int(len(variation_series) / M)
+            if num_of_elements_on_interval == variation_series[-1]:
+                M += 2
+            elif variation_series[int(num_of_elements_on_interval) + 1] == 0:
+                M -= 1
+            else:
+                break
+        else:
+            M -= 1
+
+    print("Количество элементов на интервале:", int(num_of_elements_on_interval))
+    num_of_columns = int(len(variation_series) / num_of_elements_on_interval)
+    print("Количество столбцов:", num_of_columns)
+
+    A = np.zeros(num_of_columns)
+    B = np.zeros(num_of_columns)
+    j = num_of_elements_on_interval
+    for i in range(len(A)):
+        B[i] = (variation_series[j] + variation_series[j + 1]) / 2
+        if i == 0:
+            A[i] = variation_series[0]
+        else:
+            A[i] = B[i - 1]
+        j += num_of_elements_on_interval
+        if j == len(variation_series):
+            A[i + 1] = B[i]
+            B[i + 1] = variation_series[-1]
+            break
+    print(A)
+    print(B)
+
+    column_heights = np.zeros(num_of_columns)
+    column_len = np.zeros(num_of_columns)
+    for i in range(num_of_columns):
+        column_heights[i] = num_of_elements_on_interval / (len(variation_series) * (B[i] - A[i]))
+        column_len[i] = B[i] - A[i]
+    print("Высоты столбцов:", column_heights)
+
+    fig, ax = plt.subplots()
     for i in range(int(num_of_columns)):
-        gap_height[i] = (1/num_of_columns)/(gap_lengths[i+1]-gap_lengths[i])
+        ax.add_patch(
+            patches.Rectangle(
+                (A[i], 0),
+                column_len[i],
+                column_heights[i],
+                edgecolor="red",
+            )
+        )
+    f = lambda x: (1 / (16 * np.power(x, 3 / 4)))
+    x = np.linspace(0.0005, 81)
+    plt.plot(x, f(x), "yellow")
+    plt.xlim(-1, variation_series[-1] + 1)
+    plt.ylim(0, column_heights[0] + 0.05)
+    plt.show()
 
 
-arr = random_Yi(-1, 3)
-print("Исходный ряд:", )
-variation_series = sorted(arr)
-variation_series = np.around(variation_series)
+source_row = random_Yi(-1, 3)
+variation_series = sorted(source_row)
 print("Вариационный ряд:", variation_series)
 
-frequency = Counter(variation_series)
-freq_arr = np.array([frequency[i] for i in list(set(frequency.elements()))])
-# print("Частоты:", freq_arr)
+if len(variation_series) <= 100:
+    M = int(sqrt(len(variation_series)))
+else:
+    M = int(random.randint(2, 4) * math.log(len(variation_series),10))
 
-draw_poligon(variation_series, freq_arr)
-draw_imper_func(freq_arr, arr)
-draw_equal_interval_hist(variation_series)
-#draw_equal_chance_hist(variation_series)
-plt.show()
+print("@@@@@@@@@@ РАВНОИНТЕРВАЛЬНЫЙ МЕТОД @@@@@@@@@")
+print("количество столбцов:", M)
+A = np.zeros(M)
+B = np.zeros(M)
+A, B = plot_equal_interval_hist(variation_series, M, A, B)
+plot_poligon_and_func(A, B, M, variation_series)
+
+print("@@@@@@@@@@ РАВНОВЕРОЯТНОСТНЫЙ МЕТОД @@@@@@@@@")
+plot_equal_chance_hist(M)
